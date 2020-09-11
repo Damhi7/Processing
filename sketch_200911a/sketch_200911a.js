c7f3dd75d96dde8ba4ee5f76bb3b9672b40bdb08
Damhi <70740618+Damhi7@users.noreply.github.com>
2020-09-11 15:24:42 +0900

    10104 김담희

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(500, 500);
+ 
+  
+  fill(255, 100, 100);
+  ellipse(100, 100, 50, 50);
+  fill(250, 150, 100);
+  ellipse(150,150,100,100);
+  fill(170, 100, 100);
+  ellipse(50, 50, 23, 23);
+  fill(80, 250, 200);
+  ellipse(200, 100, 85, 90);
+  fill(300, 50, 25);
+  ellipse(100, 17, 10);
+
+  
+  
+}
+
+
+function draw() {
+
+}
+
+
+
+  

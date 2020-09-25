de97b8709366a204e18b3d5c1b667d41f38787ba
Damhi <70740618+Damhi7@users.noreply.github.com>
2020-09-25 14:41:35 +0900

    jjk

---
+++ sketch_200925a/p5js-temp-sketch_200925a489471890220230364.js
@@ -0,0 +1,10 @@
+let size = 2;
+
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+}
+rect
+      let red = int(random(0, 256));
+      let green = int(random(0, 256));
+      let blue = int(random(0, 256));
